943d79a7aaccf6e1ce2614237ef0c5555cb3d56b
Gabriel <gabriel.viera4@upr.edu>
2023-07-09 00:55:37 -0700

    backup

--- Scripts/CameraControl.gd
+++ Scripts/CameraControl.gd
@@ -10,7 +10,7 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	$h.rotation_degrees.y = camrot_h
 	$h/v.rotation_degrees.x = camrot_v
 func _input(event):
